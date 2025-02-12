7129c1605e6c62619d65a041fa64eab92ee4e852
Dylan Lopez <59268334+dylanriverlopez@users.noreply.github.com>
2025-02-12 18:29:17 -0500

    update to config, added lua plugins folder

--- init.lua
+++ init.lua
@@ -18,31 +18,13 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 local opts = {}
-local plugins = {
-    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-    -- init.lua:
-    {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
--- or                              , branch = '0.1.x',
-      dependencies = { 'nvim-lua/plenary.nvim' }
-    },
-    {"nvim-treesitter/nvim-treesitter", build= ":TSUpdate"},
-    {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-    },
-  }
-}
 
-require("lazy").setup(plugins, opts)
+require("lazy").setup("plugins")
+
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>n', ':Neotree<CR>', {})
+vim.keymap.set('n', '<C-n>', ':Neotree<CR>', {})
 
 
 local config = require("nvim-treesitter.configs")
